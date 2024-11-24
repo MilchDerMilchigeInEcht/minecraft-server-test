080dc7782c769138c45e4059953fab29c4531e47
MilchDerMilchigeInEcht <105641374+MilchDerMilchigeInEcht@users.noreply.github.com>
2024-11-24 16:02:28 +0100

    Add files via upload

---
+++ server.sh
@@ -0,0 +1,6 @@
+mkdir playit
+cd playit 
+wget https://github.com/playit-cloud/playit-agent/releases/download/v0.15.26/playit-linux-amd64
+cd ..
+cd old/1.2.5
+wget https://launcher.mojang.com/v1/objects/d8321edc9470e56b8ad5c67bbd16beba25843336/server.jar
